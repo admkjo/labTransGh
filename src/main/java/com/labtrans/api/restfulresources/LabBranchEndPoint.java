7cc1bb74f4192a56d9eeaee3c14606bf5256ed81
michaelasiedu <asiedubuzi@gmail.com>
2018-03-17 18:38:47 +0000

    moreeeee meeeen

--- src/main/java/com/labtrans/api/restfulresources/LabBranchEndPoint.java
+++ src/main/java/com/labtrans/api/restfulresources/LabBranchEndPoint.java
@@ -1,14 +1,8 @@
 package com.labtrans.api.restfulresources;
 
 import com.labtrans.ejb.entities.LabBranch;
-import com.labtrans.ejb.entities.LabSession;
-import com.labtrans.ejb.entities.Patient;
+import com.labtrans.ejb.sessionbean.LabAccountBean;
 import com.labtrans.ejb.sessionbean.LabBranchBean;
-import com.labtrans.ejb.sessionbean.LabSessionBean;
-import com.labtrans.ejb.sessionbean.PatientBean;
-import com.labtrans.jwtfilter.JWTTokenNeeded;
-import com.labtrans.util.JWTokenUtility;
-import com.labtrans.util.PasswordUtils;
 import java.security.Principal;
 import java.util.Date;
 import java.util.List;
@@ -19,13 +13,8 @@ import javax.ws.rs.core.Response;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.ws.rs.core.GenericEntity;
-
-import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
 import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.Response.Status.NOT_FOUND;
-import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
 import javax.ws.rs.core.SecurityContext;
 import org.apache.commons.lang3.StringUtils;
 
@@ -42,6 +31,8 @@ public class LabBranchEndPoint {
 
     @EJB
     private LabBranchBean labBranchBean;
+    @EJB
+    private LabAccountBean labAccountBean;
     private static final Logger LOGGER = Logger.getLogger(LabBranchEndPoint.class.getName());
 
     @POST
@@ -51,12 +42,12 @@ public class LabBranchEndPoint {
             @FormParam("branchName") String branchName,
             @FormParam("contact") String contact,
             @FormParam("email") String email,
-            @FormParam("labcode") String labcode
+            @Context SecurityContext securityContext
     ) {
         try {
             //check for empty inputs
             if (StringUtils.isNotBlank(branchName) && StringUtils.isNotBlank(contact)
-                    && StringUtils.isNotBlank(email) && StringUtils.isNotBlank(labcode)) {
+                    && StringUtils.isNotBlank(email)) {
             } else {
                 return Response.ok("Please Complete All Fields").build();
             }
@@ -64,6 +55,10 @@ public class LabBranchEndPoint {
             if (dp == true) {
                 return Response.ok("This Lab is already registered in the system").build();
             }
+            Principal principal = securityContext.getUserPrincipal();
+            String username = principal.getName();
+            Integer labcode_ = labAccountBean.labAccountFindByAttribute("username", username, false).get(0).getId();
+            String labcode = String.valueOf(labcode_);
             String branchCode = "LAB-" + branchName + new Date().toString();
             LabBranch labBranch = new LabBranch();
             labBranch.setBranchCode(branchCode);
